827141527692884fc6942b49da2593ab73fb6dcb
Ubuntu <ubuntu@ip-172-31-52-87.ec2.internal>
2023-04-01 08:31:49 +0000

    add docker swarm

---
+++ docker-swarm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#docker node ls
+#docker service create --name Daily-journal-service --replicas 3 --publish 3000:3000 danish1234512/your-image-name
+#sudo docker stack deploy -c node-cluster.yaml node-stack 
+#docker node ls
+#docker stack ls 
+#sudo docker service ls
+docker service scale node-stack_db=4
